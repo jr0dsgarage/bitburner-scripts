9fa3657d6ea591072d1de229acb370517e6cbc77
j__r0d <jkozeal@gmail.com>
2023-10-11 21:19:08 -0600

    add a colors.ts file for later importing

---
+++ src/colors.ts
@@ -0,0 +1,8 @@
+/** @param {NS} ns */
+// created by j__r0d 10/11/23
+export async function main(ns: any) {
+    const cyan = "\u001b[36m";
+    const green = "\u001b[32m";
+    const red = "\u001b[31m";
+    const reset = "\u001b[0m";
+}
