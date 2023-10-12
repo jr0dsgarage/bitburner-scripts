57f9bfc056934adeba367367697754c2c9dd116a
j__r0d <jkozeal@gmail.com>
2023-10-11 21:43:58 -0600

    add colors (found on reddit)

--- src/colors.ts
+++ src/colors.ts
@@ -1,8 +1,10 @@
-
 // created by j__r0d 10/11/23
-const colors = {
-    "cyan": "\u001b[36m",
-    "green": "\u001b[32m",
-    "red": "\u001b[31m",
-    "reset": "\u001b[0m",
-};
+/** @param {NS} ns */
+export async function main(ns: any) {
+    const colors = {
+        "cyan": "\u001b[36m",
+        "green": "\u001b[32m",
+        "red": "\u001b[31m",
+        "reset": "\u001b[0m",
+    };
+}
