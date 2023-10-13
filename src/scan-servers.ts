ddb12bff3e1874b0a5d34f783a8242bbb648336a
j__r0d <jkozeal@gmail.com>
2023-10-12 23:33:52 -0600

    copilot refactor to allow listBuilder recursion

--- src/scan-servers.ts
+++ src/scan-servers.ts
@@ -3,27 +3,44 @@
 import { colors } from "./colors";
 
 /** @param {NS} ns */
-export async function main(ns: any) {
-    let serverList = await buildList(ns);
+export async function main(ns: any, depth: number) {
+    let serverList = await buildScannedServerList(ns, depth);
     ns.tprintf(`found ${colors.Cyan}${serverList.length}${colors.Reset} servers`)
-    // ns.tprintf(`${colors.Cyan}${serverList}${colors.Reset}`);
+    ns.tprintf(`${colors.Cyan}${serverList}${colors.Reset}`);
 }
-/** @param {NS} ns */
-export async function buildList(ns: any) {
-    const serverList: Array<string> = [];
-    for (const hostname of await ns.scan()) {
-        if (canAddServer(hostname, serverList)) {
-            ns.tprintf(`server found: ${colors.Cyan}${hostname}${colors.Reset}`);
-            serverList.push(hostname);
-        }
+/**
+ * 
+ * @param ns 
+ * @param depth scan depth; defaults to 1
+ * @param serverList [Optional] running list of servers (is returned at end of recursion)
+ * @param scannedServers [Optional] list of servers already scanned
+ * @returns 
+ */
+export async function buildScannedServerList(ns: any, depth: number = 1, serverList: string[] = [], scannedServers: string[] = []) {
+    ns.tprintf(`scanning servers to depth ${colors.Magenta}${depth}${colors.Reset}`);
+    if (depth <= 0) {
+        return serverList;
+    }
 
-        for (const neighborServer of await ns.scan(hostname)) {
-            if (canAddServer(neighborServer, serverList)) {
-                ns.tprintf(`server found: ${colors.Cyan}${neighborServer}${colors.Reset}`);
-                serverList.push(neighborServer);
+    const serversToScan = await ns.scan(...scannedServers);
+
+    for (const server of serversToScan) {
+        if (canAddServer(server, serverList)) {
+            ns.tprintf(`server found: ${colors.Cyan}${server}${colors.Reset}`);
+            serverList.push(server);
+        }
+    }
+
+    if (depth > 1) {
+        for (const server of serversToScan) {
+            if (!scannedServers.includes(server)) {
+                const neighborServers = await ns.scan(server);
+                const newScannedServers = [...scannedServers, server];
+                await buildScannedServerList(ns, depth - 1, serverList, newScannedServers.concat(neighborServers));
             }
         }
     }
+
     return serverList;
 }
 
@@ -45,7 +62,7 @@ export function canAddServer(serverHostname: string, serverListName: string[]) {
 };
 
 /**
- * the above function was created by Copilot after I asked a few questions about a better way to do this.
+ * the above functions were created by Copilot after I asked a few questions about a better way to do this.
  * below was my attempt....clearly I wasn't thinking in the same direction at all,
  * however copilot did use this code to generate its own code.
 export function canAddServer(serverName: string, serverListName: string[]) {
