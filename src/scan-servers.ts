bd45c7a327409fa3db60f146239e2fea4fa03ab1
j__r0d <jkozeal@gmail.com>
2023-10-12 20:16:44 -0600

    working method found, keeping for future tests

--- src/scan-servers.ts
+++ src/scan-servers.ts
@@ -1,8 +1,14 @@
 // created by j__r0d 10/12/23
+// tests to work out scanning servers via script
 import { colors } from "./colors";
 
 /** @param {NS} ns */
 export async function main(ns: any) {
-    let serverlist = ns.scan();
-    ns.tprint(serverlist);
+    let serverList = ns.scan() as string[];
+    serverList.forEach((server) => {
+        ns.scan(server).forEach((neighborServer: string) => {
+            if (!(serverList.includes(neighborServer) || neighborServer=="home")) serverList.push(neighborServer);
+        })
+    });
+    ns.tprintf(`${colors.Cyan}${serverList}${colors.Reset}`);
 }
