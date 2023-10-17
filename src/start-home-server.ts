6d2ef645462275d342518b0aef50f1f7c8e43531
j__r0d <jkozeal@gmail.com>
2023-10-16 22:29:23 -0600

    ensure one thread is always used

--- src/start-home-server.ts
+++ src/start-home-server.ts
@@ -4,12 +4,12 @@ import { colors } from "./colors";
 /** @param {NS} ns */
 export async function main(ns: any) {
     ns.tprint(`INFO: deploying hack on ${colors.Cyan}home${colors.Reset} server...`);
-    const hackToApply = ns.args[0];
+    const hackToDeploy = ns.args[0];
     let hostname = "home";
     if (ns.args[1] == "-k") {
         ns.killall();
     }
-    let threadsToUse = (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)) / ns.getScriptRam(hackToApply);
-    ns.run(hackToApply, ~~threadsToUse);
+    let threadsToUse = Math.max(1, (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)) / ns.getScriptRam(hackToDeploy));
+    ns.run(hackToDeploy, ~~threadsToUse);
     ns.tprint(`INFO: ...hack deployed using ${colors.Magenta}${~~threadsToUse}${colors.Reset} threads`);
 }
--- src/start-purchased-servers.ts
+++ src/start-purchased-servers.ts
@@ -4,13 +4,13 @@ import { colors } from "./colors";
 /** @param {NS} ns */
 export async function main(ns: any) {
     ns.tprint("INFO: deploying hack on purchased servers...");
-    const hackToApply = ns.args[0];
+    const hackToDeploy = ns.args[0];
     let i = 0;
     while (i < ns.getPurchasedServerLimit()) {
         let hostname = "pserv-".concat(i.toString());
         ns.killall(hostname);
-        let threadsToUse = (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)) / ns.getScriptRam(hackToApply);
-        ns.exec(hackToApply, hostname, ~~threadsToUse);
+        let threadsToUse = Math.max(1, (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)) / ns.getScriptRam(hackToDeploy));
+        ns.exec(hackToDeploy, hostname, ~~threadsToUse);
         ns.tprint(`INFO: ...hack deployed using ${colors.Magenta}${~~threadsToUse}${colors.Reset} threads on ${colors.Cyan}${hostname}${colors.Reset}`);
         ++i;
     }
