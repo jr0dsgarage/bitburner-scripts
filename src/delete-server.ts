312b0e16061e1f9583bbb197822b80a20b6ae12f
j__r0d <jkozeal@gmail.com>
2023-10-20 23:45:29 -0600

    flesh out deleting server script to allow flexibility

--- src/delete-server.ts
+++ src/delete-server.ts
@@ -1,7 +1,33 @@
 import { NS } from '@ns';
+import { colors } from './colors';
 /** @param {NS} ns Netscript namespace */
 
-export async function main(ns: NS) { 
-    const purchasedServer = ns.args[0].toString();
-    ns.deleteServer(purchasedServer);
+export async function main(ns: NS) {
+    if (ns.args.includes(`-a`)) {
+        deleteAllServers(ns)
+    }
+    else if (ns.args[0]) {
+        const purchasedServer = ns.args[0].toString()
+        deleteServer(ns, purchasedServer);
+    }
+    else {
+        ns.tprint(`ERROR: no server specified for deletion! ${colors.Red}(use -a to delete all purchased servers)${colors.Reset}`);
+    }
+    
+}
+
+export async function deleteServer(ns: NS, server: string) {
+    try {
+        ns.killall(server);
+        ns.deleteServer(server);
+        ns.tprint(`INFO: deleted server ${colors.Cyan}${server}${colors.Reset}`);
+    } catch {
+        ns.tprint(`ERROR: failed to delete server ${colors.Cyan}${server}${colors.Reset}`);
+    }
+}
+
+export async function deleteAllServers(ns: NS) {
+    ns.getPurchasedServers().forEach((server: string) => {
+        deleteServer(ns, server);
+    });
 }
