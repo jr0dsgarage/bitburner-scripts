ccf09b19906bbc7cf1bb25454f752a4cc57d3be6
j__r0d <jkozeal@gmail.com>
2023-10-23 12:41:25 -0600

    fix missing parenthesis in doFetch evaluation

--- src/sniff-servers.ts
+++ src/sniff-servers.ts
@@ -9,7 +9,7 @@ import { buildScannedServerList, fileFetch, getScanDepth } from './hackLib';
 export async function main(ns: NS) {
     let scanDepth = parseInt(ns.args[0].toString());
     let serverList: string[] = ns.args[1]?.toString().split(',');
-    const doFetch = ns.args.includes('-f') || ns.args.includes('-fetch') ? true : false;
+    const doFetch = (ns.args.includes('-f') || ns.args.includes('-fetch') )? true : false;
     if (isNaN(scanDepth)) scanDepth = await getScanDepth(ns);
     if (serverList.length === 0) serverList = await buildScannedServerList(ns, scanDepth);
     const homefilelist = await (async () =>  ns.ls('home'))();
