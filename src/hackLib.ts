fc7e7d82d13079c37fdc5d1893cda86ea934bdb2
j__r0d <jkozeal@gmail.com>
2023-10-23 17:19:04 -0600

    add the colors Enum to hackLib

--- src/hackLib.ts
+++ src/hackLib.ts
@@ -2,11 +2,29 @@
 // created by j__r0d 2023-10-22
 
 import { NS } from '@ns';
-import { colors } from './colors';
 
 
 export const defaultHackToDeploy = `my-first-hack.js`;
 export const defaultHackTargetHostname = `joesguns`;
+export const colors = {
+    Black: '\u001b[30m',
+    Red: '\u001b[31m',
+    Green: '\u001b[32m',
+    Yellow: '\u001b[33m',
+    Blue: '\u001b[34m',
+    Magenta: '\u001b[35m',
+    Cyan: '\u001b[36m',
+    White: '\u001b[37m',
+    BrightBlack: '\u001b[30;1m',
+    BrightRed: '\u001b[31;1m',
+    BrightGreen: '\u001b[32;1m',
+    BrightYellow: '\u001b[33;1m',
+    BrightBlue: '\u001b[34;1m',
+    BrightMagenta: '\u001b[35;1m',
+    BrightCyan: '\u001b[36;1m',
+    BrightWhite: '\u001b[37;1m',
+    Reset: '\u001b[0m',
+}
 
 /**
  * @remarks This function is a recursive function that scans servers to a given tree depth and returns a list of all servers to hack.
