7f7c0d235b507fc6bf3d8356f547b5824889c24a
j__r0d <jkozeal@gmail.com>
2023-10-28 12:47:18 -0600

    Naming fix

--- src/sniff-servers.ts
+++ src/sniff-servers.ts
@@ -1,7 +1,7 @@
 // created by j__r0d 2023-10-22
 
 import { NS } from '@ns';
-import { buildScannedServerList, fileFetch, getScanDepth } from './hackLib';
+import { buildScannedServerList, fileFetch, getMaxPossibleScanDepth } from './hackLib';
 
 /** 
  * @param {NS} ns Netscript namespace
@@ -10,7 +10,7 @@ export async function main(ns: NS) {
     let scanDepth = parseInt(ns.args[0]?.toString());
     const doFetch = (ns.args.includes('-f') || ns.args.includes('-fetch') )? true : false;
     if (isNaN(scanDepth)) 
-        scanDepth = await getScanDepth(ns);
+        scanDepth = await getMaxPossibleScanDepth(ns);
     const serverList = await buildScannedServerList(ns, scanDepth);
     const homefilelist = await (async () =>  ns.ls('home'))();
     serverList.forEach((hostname: string) => {
