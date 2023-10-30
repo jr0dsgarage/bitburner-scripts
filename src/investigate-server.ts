865c56961abdf2e76ea3237326f4969e196c3b9a
j__r0d <jkozeal@gmail.com>
2023-10-30 12:58:46 -0600

    Move updateable values into the while loop

--- src/investigate-server.ts
+++ src/investigate-server.ts
@@ -16,27 +16,29 @@ export async function main(ns: NS) {
 
     const moneyThresh = ns.getServerMaxMoney(targetHostname);
     const securityThresh = ns.getServerMinSecurityLevel(targetHostname);
-
-    const usedRam: number = ns.getServerUsedRam(targetHostname);
-    const maxRam: number = ns.getServerMaxRam(targetHostname);
     const minSecurityLevel: number = ns.getServerMinSecurityLevel(targetHostname);
-    const moneyAvailable: number = ns.getServerMoneyAvailable(targetHostname);
     const maxMoney: number = ns.getServerMaxMoney(targetHostname)
-    const securityLevel: number = ns.getServerSecurityLevel(targetHostname);
-    const requiredHackingLevel: number = ns.getServerRequiredHackingLevel(targetHostname);
     const numPortsRequired: number = ns.getServerNumPortsRequired(targetHostname);
-    const hackTime: number = ns.getHackTime(targetHostname);
-    const growTime: number = ns.getGrowTime(targetHostname);
-    const weakenTime: number = ns.getWeakenTime(targetHostname);
-
-    const availableRam: number = maxRam - usedRam;
+    const requiredHackingLevel: number = ns.getServerRequiredHackingLevel(targetHostname);
+    const maxRam: number = ns.getServerMaxRam(targetHostname);
 
     while (true) {
         ns.ui.clearTerminal();
+
+        let moneyAvailable: number = ns.getServerMoneyAvailable(targetHostname);
+        let usedRam: number = ns.getServerUsedRam(targetHostname);
+        let securityLevel: number = ns.getServerSecurityLevel(targetHostname);
         
+
+        let availableRam: number = maxRam - usedRam;
+
+        const hackTime: number = ns.getHackTime(targetHostname);
+        const growTime: number = ns.getGrowTime(targetHostname);
+        const weakenTime: number = ns.getWeakenTime(targetHostname);
+
         ns.tprintf(`Investigating ${colors.Green}${targetHostname}${colors.Reset}: ${securityLevel > securityThresh ? '👇' : moneyAvailable < moneyThresh ? '👆' : '👉'}`);
-        ns.tprintf(`RAM: ${colors.Magenta}${ns.formatNumber(usedRam,2)}GB${colors.Reset} / ${colors.Magenta}${~~maxRam}GB${colors.Reset}`);
-        ns.tprintf(`Available RAM: ${colors.Magenta}${ns.formatNumber(availableRam,2)}GB${colors.Reset}`)
+        ns.tprintf(`RAM: ${colors.Magenta}${ns.formatNumber(usedRam, 2)}GB${colors.Reset} / ${colors.Magenta}${~~maxRam}GB${colors.Reset}`);
+        ns.tprintf(`Available RAM: ${colors.Magenta}${ns.formatNumber(availableRam, 2)}GB${colors.Reset}`)
         ns.tprintf(`Security Level: ${colors.Magenta}${securityLevel}${colors.Reset}`);
         ns.tprintf(`Minimum Security Level: ${colors.Magenta}${minSecurityLevel}${colors.Reset}`);
         ns.tprintf(`Money Available: ${colors.Magenta}${moneyAvailable}${colors.Reset}`);
@@ -46,8 +48,7 @@ export async function main(ns: NS) {
         ns.tprintf(`Hack time: ${colors.Magenta}${~~hackTime}s${colors.Reset}`);
         ns.tprintf(`Grow time: ${colors.Magenta}${~~growTime}s${colors.Reset}`);
         ns.tprintf(`Weaken time: ${colors.Magenta}${~~weakenTime}s${colors.Reset}`);
-        
-        await ns.sleep(500);
-    }
 
+        await ns.sleep(1000);
+    }
 }
