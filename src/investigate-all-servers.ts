c4f6cd2d310111f76c765fa173f473d4277ce847
j__r0d <jkozeal@gmail.com>
2023-11-03 21:03:43 -0600

    Rename script; small tweaks + formatting fix

--- src/investigate-all-servers.ts
+++ src/investigate-all-servers.ts
@@ -1,3 +1,6 @@
+/* `your hacking target should be the server with highest max money that's required hacking level is under 1/2 of your hacking level.`
+ * `Keep security level low. Security level affects everything when hacking. Two important Netscript functions for this are getServerSecurityLevel() and getServerMinSecurityLevel()`
+ */
 import { NS, Server } from '@ns';
 import { ServerMatrix } from './server-matrix';
 import { colors } from './hackLib';
@@ -14,7 +17,7 @@ export async function main(ns: NS) {
     while (true) {
         ns.ui.clearTerminal();
 
-        ns.tprint(`INFO: investigating ${matrix.fullScannedServerList.length} servers...`);
+        ns.tprintf(`INFO: investigating ${matrix.fullScannedServerList.length} servers...`);
         matrix.fullScannedServerList.forEach((targetableServer: Server) => {
             const targetHostname: string = targetableServer.hostname;
             const securityLevel: number = ns.getServerSecurityLevel(targetHostname);
@@ -27,21 +30,20 @@ export async function main(ns: NS) {
             const growTime: number = ns.getGrowTime(targetHostname);
             const weakenTime: number = ns.getWeakenTime(targetHostname);
 
+            const playerHackingLevel = ns.getHackingLevel();
+            const requiredHackingLevel: number = ns.getServerRequiredHackingLevel(targetHostname);
+
             
             const hostnameString = `${colors.Green}${targetHostname}${colors.Reset}:${securityLevel > minSecurityLevel ? '👇' : moneyAvailable < maxMoney ? '👆' : '👉'}`;
+            const moneyString = `${colors.Magenta}$${ns.formatNumber(moneyAvailable).padEnd(8, ' ')}/$${ns.formatNumber(maxMoney)}${colors.Reset}`;
             const securityString = `S:${statusColor}${ns.formatNumber(securityLevel, 3)}${colors.Reset}`;
             const hackString = `H:${statusColor}${(~~hackTime)}${colors.Reset}`;
             const growString = `G:${statusColor}${(~~growTime)}${colors.Reset}`;
             const weakenString = `W:${statusColor}${(~~weakenTime)}${colors.Reset}`;
-            const moneyString = `${colors.Magenta}$${ns.formatNumber(moneyAvailable)}/$${ns.formatNumber(maxMoney)}${colors.Reset}`;
+            
+            const targetString = targetHostname === matrix.getRichestServerHostname() && requiredHackingLevel < (playerHackingLevel / 2) ? `🎯` : ``;
 
-            ns.tprintf(`${hostnameString.padEnd(30, `.`)}
-                        ${moneyString.padEnd(30, `.`)}
-                        ${securityString.padEnd(20, `.`)}
-                        ${hackString.padEnd(20, `.`)}
-                        ${growString.padEnd(20, `.`)}
-                        ${weakenString.padEnd(20, `.`)}`
-            );
+            ns.tprintf(`${hostnameString.padEnd(30, `.`)}${moneyString.padEnd(30, `.`)}${securityString.padEnd(20, `.`)}${hackString.padEnd(20, `.`)}${growString.padEnd(20, `.`)}${weakenString.padEnd(20, `.`)}${targetString}`);
         });
         await ns.sleep(10000);
     }
