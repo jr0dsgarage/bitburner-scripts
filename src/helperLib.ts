d8ed5371942930f89359bbb3e09802160d4608e7
j__r0d <jkozeal@gmail.com>
2023-11-08 16:45:43 -0700

    Switch colors const to TerminalFormats class - allows future inclusion of non-color formats, ie bold/italic etc

--- src/helperLib.ts
+++ src/helperLib.ts
@@ -1,25 +1,8 @@
-export const colors = {
-    Black: '\u001b[30m',
-    Red: '\u001b[31m',
-    Green: '\u001b[32m',
-    Yellow: '\u001b[33m',
-    Blue: '\u001b[34m',
-    Magenta: '\u001b[35m',
-    Cyan: '\u001b[36m',
-    White: '\u001b[37m',
-    BrightBlack: '\u001b[30;1m',
-    BrightRed: '\u001b[31;1m',
-    BrightGreen: '\u001b[32;1m',
-    BrightYellow: '\u001b[33;1m',
-    BrightBlue: '\u001b[34;1m',
-    BrightMagenta: '\u001b[35;1m',
-    BrightCyan: '\u001b[36;1m',
-    BrightWhite: '\u001b[37;1m',
-    Reset: '\u001b[0m',
-};
-
+// Defaults that can be used throughout scripts
 export const defaultHackToDeploy = `my-first-hack.js`;
 export const defaultHackTargetHostname = `joesguns`;
+
+// Programs that can open ports
 export const portOpeningPrograms = [
     `brutessh.exe`,
     `ftpcrack.exe`,
@@ -28,6 +11,28 @@ export const portOpeningPrograms = [
     `sqlinject.exe`
 ];
 
-export function colorize(str: string, color: string) {
-    return `${color}${str}${colors.Reset}`;
+export type Color = string;
+
+export class TerminalFormats {
+    static Reset: Color = '\u001b[0m';
+    static Black: Color = '\u001b[30m';
+    static Red: Color = '\u001b[31m';
+    static Green: Color = '\u001b[32m';
+    static Yellow: Color = '\u001b[33m';
+    static Blue: Color = '\u001b[34m';
+    static Magenta: Color = '\u001b[35m';
+    static Cyan: Color = '\u001b[36m';
+    static White: Color = '\u001b[37m';
+    static BrightBlack: Color = '\u001b[30;1m';
+    static BrightRed: Color = '\u001b[31;1m';
+    static BrightGreen: Color = '\u001b[32;1m';
+    static BrightYellow: Color = '\u001b[33;1m';
+    static BrightBlue: Color = '\u001b[34;1m';
+    static BrightMagenta: Color = '\u001b[35;1m';
+    static BrightCyan: Color = '\u001b[36;1m';
+    static BrightWhite: Color = '\u001b[37;1m'; 
+}
+
+export function colorize(value: any, color: Color) {
+    return `${color}${value}${TerminalFormats.Reset}`;
 }
