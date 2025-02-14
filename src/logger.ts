b3ca45fe7f68f077128c7b2501e5393c7d303b74
j__r0d <jkozeal@gmail.com>
2025-02-13 17:11:37 -0700

    make logger use helperLib for colors; colorTable helps find 256-color variants

--- src/logger.ts
+++ src/logger.ts
@@ -1,48 +1,35 @@
+import { TerminalFormats as colors, colorize } from './helperLib';
+
 /*
 * Logger class to log messages to the terminal with different log levels.
-* 
-* Doesn't function yet, this is just a placeholder for future development.
 */
 
-enum LogLevel {
-    INFO = 'INFO',
-    DEBUG = 'DEBUG',
-    ERROR = 'ERROR'
-}
+type LogLevel = 'INFO' | 'DEBUG' | 'ERROR';
+
+const logLevelColors: { [key in LogLevel]: string } = {
+    INFO: colors.Info,
+    DEBUG: colors.Debug,
+    ERROR: colors.Error
+};
 
 class Logger {
-    private static getColor(level: LogLevel): string {
-        switch (level) {
-            case LogLevel.INFO:
-                return '\x1b[34m'; // Blue
-            case LogLevel.DEBUG:
-                return '\x1b[33m'; // Yellow
-            case LogLevel.ERROR:
-                return '\x1b[31m'; // Red
-            default:
-                return '\x1b[0m'; // Reset
-        }
-    }
-
     private static formatMessage(level: LogLevel, message: string, ...variables: any[]): string {
-        const color = Logger.getColor(level);
-        const reset = '\x1b[0m';
-        const formattedVariables = variables.map(variable => `${color}${variable}${reset}`);
-        return `${color}[${level}]${reset} ${message} ${formattedVariables.join(' ')}`;
+        const formattedVariables = variables.map(variable => colorize(variable, logLevelColors[level]));
+        return `${colorize(`[${level}] ${message}`, logLevelColors[level])} ${formattedVariables.join(' ')}`;
     }
 
     static info(ns: any, message: string, ...variables: any[]): void {
-        ns.tprint(Logger.formatMessage(LogLevel.INFO, message, ...variables));
+        ns.tprint(Logger.formatMessage('INFO', message, ...variables));
     }
 
     static debug(ns: any, message: string, DEBUG: boolean, ...variables: any[]): void {
         if (DEBUG) {
-            ns.tprint(Logger.formatMessage(LogLevel.DEBUG, message, ...variables));
+            ns.tprint(Logger.formatMessage('DEBUG', message, ...variables));
         }
     }
 
     static error(ns: any, message: string, ...variables: any[]): void {
-        ns.tprint(Logger.formatMessage(LogLevel.ERROR, message, ...variables));
+        ns.tprint(Logger.formatMessage('ERROR', message, ...variables));
     }
 }
 
